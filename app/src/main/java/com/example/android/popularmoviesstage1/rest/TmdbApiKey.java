9bc2d6ee2d8e7ba156842cadff20eea6ecf83ea5
Hrishikesh Kadam <hrkadam.92@gmail.com>
2017-10-11 01:01:34 +0530

    Copy paste your api_key in this class

---
+++ app/src/main/java/com/example/android/popularmoviesstage1/rest/TmdbApiKey.java
@@ -0,0 +1,9 @@
+package com.example.android.popularmoviesstage1.rest;
+
+/**
+ * Created by Hrishikesh Kadam on 11/10/2017
+ */
+
+public class TmdbApiKey {
+    public static final String api_key = null;
+}
